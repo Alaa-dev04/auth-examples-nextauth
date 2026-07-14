cca4335f6d3a060f9d15ac94aa3aaa2f02748e94
Alaa-dev04 <nahea906.a@gmail.com>
2026-07-14 16:48:48 +0300

    CredentialsProvider

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,41 @@
+
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { getuserbyemail } from "./data/user";
+
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
 } = NextAuth({
+    session : {strategy:"jwt"},
   providers: [
+    CredentialsProvider({
+        credentials:{
+            email:{},
+            password:{},
+        },
+        async authorize(credentials) {
+            if (credentials === null ) return null;
+            try{
+                const user = getuserbyemail(credentials?.email as string );
+                if (user){
+                    const isMatch = user?.password === credentials?.password ;
+                    if (isMatch) {return user ;} else{
+                          throw new Error("check your password ");
+                    }
+                }else{
+                    throw new Error("user not found ");
+                }
+
+            } catch (error){
+                throw new Error();
+            }
+        }
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
