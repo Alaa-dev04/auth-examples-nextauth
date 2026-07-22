548d14f35c155b7b34ed692e44c3bf43d29f6573
Alaa-dev04 <nahea906.a@gmail.com>
2026-07-22 14:23:53 +0300

    this is the new structure

---
+++ src/app/(main)/products/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductListPage = () => {
+  return (
+    <div>
+        {/* iam gonna put here the products component */}
+      
+    </div>
+  )
+}
+
+export default ProductListPage
